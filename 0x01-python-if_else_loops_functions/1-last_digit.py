361bd92aaebae9465f67d5ddc86b27616af6487c
emmanueladdo <asememmanueladdo@gmail.com>
2023-05-09 20:43:05 -0700

    the checker for negative or positive

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 ltdi = number % 10 if number > 0 else int(repr(number)[-1]) * -1
 if (ltdi > 5):
-    print(f"Last digit of {number:d} is {lstdi:d} and is greater than 5")
+    print(f"Last digit of {number:d} is {ltdi:d} and is greater than 5")
 elif (ltdi == 0):
     print(f"Last digit of {number:d} is {ltdi:d} and is 0")
 else:
