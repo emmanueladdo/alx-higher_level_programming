7d0764d6e4452e79c96db58848b72d57889a17d5
emmanueladdo <asememmanueladdo@gmail.com>
2023-05-11 10:07:16 -0700

    additio

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
-
-a = 1
-b = 2
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
 
 results = add(a, b)
 print("{} + {} = {}".format(a, b, results))
