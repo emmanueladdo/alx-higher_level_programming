e7c379bd884de43e2f24f3f4bb7171f44778040a
emmanueladdo <asememmanueladdo@gmail.com>
2023-05-18 06:02:17 -0700

    complex deletion

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(my_dict, value):
     dulp = my_dict.copy()
-    for key, value in dulp.items():
-        if item in value:
+    for key, val in dulp.items():
+        if value in val:
             del my_dict[key]
     return my_dict
