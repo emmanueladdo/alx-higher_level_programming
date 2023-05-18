061935dc8095e45647bb2a259bfa0406a7aad210
emmanueladdo <asememmanueladdo@gmail.com>
2023-05-18 05:48:28 -0700

    weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,5 +3,5 @@ def weight_average(my_list=[]):
     if not mylist:
         return 0
     a = list(map(list, zip(*my_list)))
-    b = [x * y for x, y in zip(a[0], b[1])]
+    b = [x * y for x, y in zip(a[0], a[1])]
     return sum(b) / sum(a[1])
