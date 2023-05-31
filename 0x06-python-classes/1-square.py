ceac9e500171162b35b744eca6a9ec4f1de4d250
emmanueladdo <asememmanueladdo@gmail.com>
2023-05-30 18:48:40 -0700

    Square initialize with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is a "Square"  module.
+This  provides a simple Square class with initialize size.
+"""
+
+
+class Square:
+    """Class that defines a Square by size"""
+    def __init__(self, size):
+        self._size = size
