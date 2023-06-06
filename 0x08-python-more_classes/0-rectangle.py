35c6520a5415bdba6562861b26855e9fa49ab764
emmanueladdo <asememmanueladdo@gmail.com>
2023-06-05 20:32:56 -0700

    correct

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is  "Rectangle"  module.
+
+This module provides a simple Rectangle class.
+"""
+
+
+class Rectangle:
+    """A simple Rectangle class"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This is for the project for python classes
