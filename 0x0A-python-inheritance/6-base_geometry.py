b49313f00841701bbcc36b97457616b45c54badc
emmanueladdo <asememmanueladdo@gmail.com>
2023-06-12 21:34:15 -0700

    Not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""geometry module that defines the class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represents base geometry."""
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
