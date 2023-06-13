68fd199cbafe1b90c5ace16279df2a06ec0d12d2
emmanueladdo <asememmanueladdo@gmail.com>
2023-06-13 10:16:56 -0700

    int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """module with class MyInt that inherits from int."""
+
+
 class MyInt(int):
-     """int operators == and !=."""
+    """int operators == and !=."""
     def __eq__(self, other):
         return self - other != 0
 
