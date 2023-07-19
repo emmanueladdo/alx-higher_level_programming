850b4e0193651fa30d4ab3c3aec686842340e205
root <root@DESKTOP-KUT53MR>
2023-07-19 02:12:07 +0000

    groups

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -3,7 +3,6 @@
 -- Result should display the score and
 -- number of records for this score with label 'number'
 -- List should be sorted by number of records descending
--- db name will be passed as arg to mysql cmd
 SELECT score, COUNT(*) AS number FROM second_table
 GROUP BY score
 ORDER BY number DESC;
