59adbcba21cf5aa30aa2e5703b7fb7704c46376c
root <root@DESKTOP-KUT53MR>
2023-07-19 01:26:19 +0000

    prints full describtion of table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Prints the full description of 'first_table' from db 'hbtn_0c_0'
+-- db name will be passed as an arg to mysql 
+-- Not allowed to use DESCRIBE or EXPLAIN
+SHOW CREATE TABLE first_table;
