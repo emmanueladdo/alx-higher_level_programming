c493c2cbc67bc9433f3d1ff3fd977c8580d4df45
root <root@DESKTOP-KUT53MR>
2023-07-19 01:57:14 +0000

    update value by field name

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,6 +1,6 @@
--- Update score of Bob to 10 from 'second_table' of 'hbtn_0c_0'
+-- Update score of Bob to 10 from 'second_table' of db 'hbtn_0c_0'
 -- Not allowed to use Bob's id value, only name field
 -- db name will be passed as arg to mysql cmd
 UPDATE second_table
-WHERE name = "Bob"
-SET score = 10;
+SET score = 10
+WHERE name = "Bob";
