f6e1b6b4a2d977d414b7ef6066fb964e86626256
root <root@DESKTOP-KUT53MR>
2023-07-20 02:44:09 +0000

    the made

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- If show doesn't have a genre, display NULL in genre column
 -- Each record should display tv_shows.title, tv_genres.name
 -- Results must be sorted in ascending order by show title
-SELECT s.title, g.name
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows s
 LEFT JOIN tv_show_genres m ON s.id = m.show_id
 LEFT JOIN tv_genres g ON m.genre_id = g.id
