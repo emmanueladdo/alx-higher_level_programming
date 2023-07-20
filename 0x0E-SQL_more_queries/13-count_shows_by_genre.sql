a3912e3c979fd2972287136d92481be08556e76c
root <root@DESKTOP-KUT53MR>
2023-07-20 02:20:59 +0000

    number of show by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- Each record should display tv_genres.name, number of shows
 -- Don't display a genre that doesn't have any shows linked
 -- Results must be sorted descending order by number of shows linked
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY number_shows DESC;
+ORDER BY number_of_shows DESC;
