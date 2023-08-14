d1f839036aa4cb57903b25cf267d1c674b9780c4
root <root@DESKTOP-KUT53MR>
2023-08-14 21:13:56 +0000

    the name

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const argumentNum = process.argv.length - 2;
 if (argumentNum == 1) {
 	console.log('Argument found');
-} else if (argumentNum > 1) {
+} else if (argumentNum >= 2) {
 	console.log('Arguments found');
 } else {
 	console.log('No Argument');
