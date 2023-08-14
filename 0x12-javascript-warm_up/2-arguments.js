3c813fb1c5b2af5d39c48af73b8b5ef780bb7585
root <root@DESKTOP-KUT53MR>
2023-08-14 21:26:19 +0000

    rgs

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,8 @@
 const argumentNum = process.argv.length - 2;
 if (argumentNum === 1) {
 	console.log('Argument found');
-} else if (argumentNum >= 2) {
-	console.log('Arguments found');
-} else {
+} else if (argumentNum === 0) {
 	console.log('No argument');
+} else {
+	console.log('Arguments found');
 }
