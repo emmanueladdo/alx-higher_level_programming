54ef80b9748079c84813af7c587d617bd8ae7ce9
root <root@DESKTOP-KUT53MR>
2023-08-14 21:18:48 +0000

    arguements

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const argumentNum = process.argv.length - 2;
-if (argumentNum == 1) {
+if (argumentNum === 1) {
 	console.log('Argument found');
 } else if (argumentNum >= 2) {
 	console.log('Arguments found');
 } else {
-	console.log('No Argument');
+	console.log('No argument');
 }
 
