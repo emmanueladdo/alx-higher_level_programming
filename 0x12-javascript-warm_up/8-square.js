45fa37af706d771faa360295e84938a09e8ffb38
root <root@DESKTOP-KUT53MR>
2023-08-14 22:18:08 +0000

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const num = parseInt(process.argv[2]);
-if (isNaN(num)) {
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < num; i++) {
-	  console.log('X'.repeat(num));
+    console.log('X'.repeat(num));
   }
 }
