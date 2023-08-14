955dfaad07602723c2463a85738a030b2b146da8
root <root@DESKTOP-KUT53MR>
2023-08-14 22:41:24 +0000

    biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ if (process.argv.length > 3) {
   const arg = process.argv.map(Number);
   arg.slice(2);
   arg.sort((a, b) => a - b);
-  console.log(args[arg.length - 2]);
+  console.log(arg[arg.length - 2]);
 } else {
   console.log(0);
 }
