7855464c38e0bcccb2604c388caefa2f3e07de40
root <root@DESKTOP-KUT53MR>
2023-08-15 22:49:30 +0000

    occurences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * This defines a function that return.
+ * number of occurences
+ */
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count += 1;
+    }
+  }
+  return count;
+};
