b7134f692b8e79ea92761e7f4208d03275ca7a07
root <root@DESKTOP-KUT53MR>
2023-08-15 15:33:25 +0000

    retangle 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/**
+ *defines a Rectangle class.
+ *Instance attribute width with the value of w
+ *Instance attribute height with the value of h
+ */
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
