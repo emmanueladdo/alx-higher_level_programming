122d825945d2d142eb8c8feb29224a9f36da0770
root <root@DESKTOP-KUT53MR>
2023-08-15 22:22:42 +0000

    square 0

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -6,8 +6,8 @@
  *takes 1 arg "size"
  */
 
-module.exports = class Square extends require('./4-rectangle'){
-	constructor (size){
-		super(size, size);
-	}
+module.exports = class Square extends require('./4-rectangle') {
+  constructor (size) {
+    super(size, size);
+  }
 };
