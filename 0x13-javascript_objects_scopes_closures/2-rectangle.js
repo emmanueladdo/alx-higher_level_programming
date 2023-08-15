3f2b7860fa9c37242b941b31bcbd7597b5f72f34
root <root@DESKTOP-KUT53MR>
2023-08-15 16:10:29 +0000

    object

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,13 +4,14 @@
  *defines a Rectangle class.
  *Instance attribute width with the value of w
  *Instance attribute height with the value of h
+ *creates an empty object  if w or h is zero or negative
  */
 
 module.exports = class Rectangle {
   constructor (w, h) {
-	  if (w > 0 && h > 0) {
-		  this.width = w;
-		  this.height = h;
-	  }
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 };
