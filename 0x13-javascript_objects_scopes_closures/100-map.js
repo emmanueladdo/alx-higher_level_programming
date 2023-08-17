073c9a44152adead00eb1b8753fe004252e1cd16
root <root@DESKTOP-KUT53MR>
2023-08-17 08:37:09 +0000

    add map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -9,14 +9,5 @@
  * Both the initial list and the new list are printed
  */
 
-// Import the array from the file 100-data.js
-const { list } = require('./100-data');
-
-// Compute a new array using the map function
-const newList = list.map((value, index) => value * index);
-
-// Print the initial list
-console.log('Initial List:', list);
-
-// Print the new list
-console.log('New List:', newList);
+console.log(require('./100-data.js').list);
+console.log((require('./100-data.js').list).map((value, index) => value * index));
