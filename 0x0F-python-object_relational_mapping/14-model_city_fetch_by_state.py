4d1754a861b7d06038e9fb8e914b5d994034f51b
root <root@DESKTOP-KUT53MR>
2023-08-20 21:42:57 +0000

    t

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
-""" 
+"""
 Print all City objs from db 'hbtn_0e_14_usa'
 Sort in ascending order by cities.id
 Display results as "<state name>: (<city id>) <city name>"
 Script should take 3 args: username, pw, and db name
 """
 
-import sys
+from sys import
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, relationship
-
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
-
-if __name__ == '__main__':
-    if len(sys.argv) >= 4:
-        user = sys.argv[1]
-        pword = sys.argv[2]
-        db_name = sys.argv[3]
-        DATABASE_URL = 'mysql://{}:{}@localhost:3306/{}'.format(
-            user, pword, db_name
-        )
-        engine = create_engine(DATABASE_URL)
-        State.cities = relationship('City', back_populates='state')
-        Base.metadata.create_all(engine)
-        session = sessionmaker(bind=engine)()
-        result = session.query(City).order_by(City.id.asc()).all()
-        for row in result:
-            print('{}: ({}) {}'.format(row.state.name, row.id, row.name))
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for city, state in session.query(City, State).join(State).all():
+        print(f"{state.name}: ({city.id}) {city.name}")
+    session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -8,27 +8,10 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from model_state import Base
 
-Base = declarative_base()
-"""Represents the base class for all tables"""
-
 
 class City(Base):
     """Represents a row in a cities table"""
-    __tablename__ = "cities"
-    id = Column(
-        Integer,
-        autoincrement=True,
-        unique=True,
-        nullable=False,
-        primary_key=True
-    )
-    name = Column(
-        String(length=128),
-        nullable=False
-    )
-    state_id = Column(
-        Integer,
-        ForeignKey('states.id'),
-        nullable=False
-    )
-    state = relationship('State', back_populates='cities')
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
