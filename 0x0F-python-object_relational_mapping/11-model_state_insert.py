16de06ae58195a232e75e8cabd3de875e061c2cf
root <root@DESKTOP-KUT53MR>
2023-08-20 05:25:29 +0000

    add l to state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -9,15 +9,13 @@ from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+                           format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
     new_state = State(name='Louisiana')
     session.add(new_state)
     session.commit()
-
-    instance_new = session.query(State).filter_by(name='Louisiana').first()
-    print(instance_new.id)
+    new_instance = session.query(State).filter_by(name='Louisiana').first()
+    print(new_instance.id)
     session.close()
