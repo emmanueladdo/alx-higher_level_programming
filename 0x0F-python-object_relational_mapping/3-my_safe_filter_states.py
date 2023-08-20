287a06927f31688b354189501cf82778335e667e
root <root@DESKTOP-KUT53MR>
2023-08-20 02:16:15 +0000

    prints argument safely

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+lists all the states from the database
+that matches with argument passed
+in a safe way
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cursor = db.cursor()
+    s_name = argv[4]
+    cursor.execute("SELECT * FROM states "
+                   "WHERE name = %(name)s "
+                   "ORDER BY id ASC", {'name':s_name})
+    for state in cursor.fetchall():
+        print(state)
+    cursor.close()
+    db.close()
