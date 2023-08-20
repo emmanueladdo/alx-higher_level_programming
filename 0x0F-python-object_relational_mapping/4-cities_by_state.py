fd937c4af01e8dc4e2ca65149438ca727ff73f26
root <root@DESKTOP-KUT53MR>
2023-08-20 02:35:21 +0000

    prints cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,9 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM cities ORDER BY id ASC")
+    cursor.execute("""SELECT cities.id, cities.name, states.name
+                      FROM cities INNER JOIN states
+                      ON cities.state_id = states.id""")
     for city in cursor.fetchall():
         print(city)
     cursor.close()
