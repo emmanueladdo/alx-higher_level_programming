5fbcaf719403dfa804daf0dbe0da65986952699e
root <root@DESKTOP-KUT53MR>
2023-08-20 21:55:41 +0000

    th

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,7 +7,7 @@ Script should take 3 args: username, pw, and db name
 """
 
 
-import sys
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
@@ -16,9 +16,9 @@ from model_city import City
 if __name__ == "__main__":
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]),
+            argv[1],
+            argv[2],
+            argv[3]),
         pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
