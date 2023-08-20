d67c3f2b270c516f17e60cf435e86f85fe1f6f21
root <root@DESKTOP-KUT53MR>
2023-08-20 05:55:43 +0000

    changes at index

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -11,13 +11,12 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    session.query(State).filter(State.id == 2).update(
-        {State.name: 'New Mexico'},
-        synchronize_session=False
-    )
+    res = session.query(State).filter(State.id == 2)
+    res.update({"name": ("New Mexico")})
+
     session.commit()
