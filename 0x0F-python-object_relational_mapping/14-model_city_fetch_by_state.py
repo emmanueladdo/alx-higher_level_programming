f6f6d818a8d08a39faf20952699c078cd7226339
root <root@DESKTOP-KUT53MR>
2023-08-20 21:23:00 +0000

    cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -5,7 +5,7 @@
 # Script should take 3 args: username, pw, and db name
 
 from sys import argv
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import create_engine
 from model_state import Base, State
 from model_city import City
@@ -13,8 +13,10 @@ from model_city import City
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(argv[1], argv[2], argv[3]))
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    for i in session.query(State, City).filter(State.id == City.state_id):
-        print("{}: ({:d}) {}".format(i.State.name, i.City.id, i.City.name))
+    State.cities = relationship('City', back_populates='state')
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)()
+    result = session.query(City).order_by(City.id.asc()).all()
+    for row in result:
+        print('{}: ({}) {}'.format(row.state.name, row.id, row.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -7,7 +7,7 @@ And an instance Base = declarative
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+from sqlalchemy.orm import relationship
 from model_state import Base
 
 Base = declarative_base()
@@ -15,16 +15,22 @@ Base = declarative_base()
 
 
 class City(Base):
-    """Representation of a row in a city table"""
-    __tablename__ = 'cities'
-    id = Column(Integer,
-                autoincrement=True,
-                unique=True,
-                nullable=False,
-                primary_key=True)
-    name = Column(String(128), nullable=False)
+    """Represents a row in a cities table"""
+    __tablename__ = "cities"
+    id = Column(
+        Integer,
+        autoincrement=True,
+        unique=True,
+        nullable=False,
+        primary_key=True
+    )
+    name = Column(
+        String(length=128),
+        nullable=False
+    )
     state_id = Column(
-            Integer,
-            ForeignKey('states.id'),
-            nullable=False
-            )
+        Integer,
+        ForeignKey('states.id'),
+        nullable=False
+    )
+    state = relationship('State', back_populates='cities')
