f4c1f63794b33be3de0191d097129ea03215ca4e
root <root@DESKTOP-KUT53MR>
2023-08-20 03:02:35 +0000

    please cities under states

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+lists all the cities from the database
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cursor = db.cursor()
+    s_name = argv[4]
+    cursor.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %(name)s ORDER BY cities.id ASC", {'name': s_name})
+    cities = cursor.fetchall()
+    print(", ".join(city[0] for city in cities))
+    cursor.close()
+    db.close()
