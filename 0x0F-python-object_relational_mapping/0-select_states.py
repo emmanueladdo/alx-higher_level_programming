e9a8caec3c36975ab7a32adabea8f7c43c13e408
root <root@DESKTOP-KUT53MR>
2023-08-20 00:55:18 +0000

    prints all state

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
     for state in cursor.fetchall():
         print(state)
     cursor.close()
