aa3940a4c9c1a69ca37dd4e9dc6d2944978ff3db
root <root@DESKTOP-KUT53MR>
2023-08-20 22:04:32 +0000

    corrected print format

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -25,6 +25,6 @@ if __name__ == "__main__":
 
     cities = session.query(City).order_by(City.id).all()
     for city in cities:
-        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
 
     session.close()
