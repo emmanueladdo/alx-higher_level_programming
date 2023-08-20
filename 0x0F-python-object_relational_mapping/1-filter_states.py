b33bec085f9515dbad3ea4632fbf83a72e2bcf65
root <root@DESKTOP-KUT53MR>
2023-08-20 01:00:29 +0000

    Prints all states starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+lists all the states from the database
+with the name starting Upper N
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    for state in cursor.fetchall():
+        print(state)
+    cursor.close()
+    db.close()
