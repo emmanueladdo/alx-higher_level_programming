565b015ff068cd3a6a157c8128eb804c1bff7cfe
root <root@DESKTOP-KUT53MR>
2023-08-22 04:34:56 +0000

    the list

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Creates  the State Califonia
+with city San Fracisco from the
+Database hbtn_0e_100_usa
+"""
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1],
+            argv[2],
+            argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        for city_instance in instance.cities:
+            print(city_instance.id, city_instance.name, sep=": ", end="")
+            print(" -> " + instance.name)
