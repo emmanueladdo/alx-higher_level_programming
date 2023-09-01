56001591f7a078477a8846d1a59e33280242befa
root <root@DESKTOP-KUT53MR>
2023-09-01 23:03:57 +0000

    hearder

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Take in a URL, sends request to URL and display value of `X-Request-Id
+from sys import argv
+from urllib.request import request
+
+if __name__ == "__main__":
+    with request.urlopen(argv[1]) as res:
+        print(res.info()['X-Request-Id'])
