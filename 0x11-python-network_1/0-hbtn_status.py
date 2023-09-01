de1f0dfb8722a8c66925f6dfd9c06b04efd5b46b
root <root@DESKTOP-KUT53MR>
2023-09-01 22:42:14 +0000

    get statu

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@ import urllib.request as request
 
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as urlresponse:
-        if urlresponse.readable():
-            data = urlresponse.read()
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        if response.readable():
+            data = response.read()
             print("Body response:")
             print("\t- type: {}".format(type(data)))
             print("\t- content: {}".format(data))
