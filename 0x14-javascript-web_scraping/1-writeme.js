91f93489c6299670ce099c60df6743be56f32584
emmanueladdo <asememmanueladdo@gmail.com>
2023-09-26 21:29:55 +0000

    write file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -19,7 +19,4 @@ if (!filePath) {
 fs.writeFile(filePath, content, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(`"${content}" was successfully written to ${filePath}`);
-  }
 });
