ccf3ffe86b23fec52afebe09e7a9789bec32d916
emmanueladdo <asememmanueladdo@gmail.com>
2023-09-26 21:18:02 +0000

    script for read me

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,16 +5,16 @@
  * If an error occurred, print the error object
  */
 
-const fs = require('fs'); // Fixed the 'require' statement and 'fs' variable
+const fs = require('fs');
 
 const filePath = process.argv[2];
 
 if (!filePath) {
-  console.error('Usage: ./0-readme.js <file-path>'); // Fixed the error message
+  console.error('Usage: ./0-readme.js <file-path>');
   process.exit(1);
 }
 
-fs.readFile(filePath, 'utf-8', (err, data) => { // Fixed the 'err.' typo
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
   } else {
