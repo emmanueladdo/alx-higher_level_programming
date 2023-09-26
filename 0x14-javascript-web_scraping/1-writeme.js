e5fc52faad184ad7a32abe1959b4825d6ae28d60
emmanueladdo <asememmanueladdo@gmail.com>
2023-09-26 21:27:33 +0000

    write file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+/* The file reads and prints a file
+ * The first arg is file path
+ * The content of the file in utf-8
+ * If an error occurred, print the error object
+ */
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+const content = process.argv[3];
+
+if (!filePath) {
+  console.error('Usage: ./0-readme.js <file-path>');
+  process.exit(1);
+}
+
+fs.writeFile(filePath, content, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`"${content}" was successfully written to ${filePath}`);
+  }
+});
