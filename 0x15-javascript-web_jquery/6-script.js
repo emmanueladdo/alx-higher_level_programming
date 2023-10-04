ff591d9f2c72fe57e7fbd25f2c6801e29a75f2ed
emmanueladdo <asememmanueladd@gmail.com>
2023-10-04 16:42:36 +0000

    change text of a div

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+/* JavaScript script cript that
+updates the text of the <header> element to New Header
+ou must use the JQuery API
+*/
+$('DIV#update_header').click(function () {
+  $('header').text('New Header!!!');
+});
