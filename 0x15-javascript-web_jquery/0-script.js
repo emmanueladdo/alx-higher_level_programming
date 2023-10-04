e8d4e1010cecf7d95c95b4391c5be9cd980638a0
emmanueladdo <asememmanueladd@gmail.com>
2023-10-04 15:26:31 +0000

    red

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,4 +1,4 @@
 /* JavaScript script that updates the
 text color of the <header> element to red (#FF0000)
 */
-document.querySelector('header').style.color = '#FF000';
+document.querySelector('header').style.color = '#FF0000';
