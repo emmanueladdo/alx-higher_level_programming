a91357ca36adbd87d1f8d03d42d37b9ef97a90c1
emmanueladdo <asememmanueladd@gmail.com>
2023-10-05 14:34:50 +0000

    the changed of color

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+/*scrpt that updates the text of <header> 
+color to read
+you use document.querySelector
+*/
+document.addEventListener('DOMContentLoaded', function () {
+  let headerElement = document.querySelector('header');
+  headerElement.style.color = '#FF0000';
+});
