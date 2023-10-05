5d21840f8d55844d2d8d45f4408c46f748a12d70
emmanueladdo <asememmanueladd@gmail.com>
2023-10-05 14:49:46 +0000

    list, add, remove

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,5 +1,17 @@
-$(document).ready(function () {
-  $('DIV#add_item').click(function () {
+$(document).ready(function() {
+  // Add item to the list
+  $('DIV#add_item').click(function() {
     $('UL.my_list').append('<li>Item</li>');
   });
+
+  // Remove the last item from the list
+  $('DIV#remove_item').click(function() {
+    $('UL.my_list li:last-child').remove();
+  });
+
+  // Clear the entire list
+  $('DIV#clear_list').click(function() {
+    $('UL.my_list').empty();
+  });
 });
+
